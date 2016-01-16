d338c3fbec921569acf34454518a0386176a55cd
Eliyahu Shlieserman <eliyahushli@gmail.com>
2016-01-16 12:33:23 +0200

    Rearrange

--- TesterConsole/Program.cs
+++ TesterConsole/Program.cs
@@ -1,9 +1,5 @@
-﻿using ClassInheritance;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using TestFields.ClassInheritance;
 
 namespace TesterConsole
 {
